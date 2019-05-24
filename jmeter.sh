8b58c11e2914960a5c867fd35e2cd595fff08916
romo <robert.moore@zuhlke.com>
2019-05-24 14:46:30 +0100

    Add a comment to explain set -e

--- jmeter.sh
+++ jmeter.sh
@@ -1,7 +1,9 @@
 #/bin/bash
+
+# set -e : This means that the bash script will exit immediately with a non-zero exit code if any command following it exits with a non-zero exit code. The set +e undoes this behaviour.
 set -e
 python3 build-test-plan.py
-python3 configure-cc-sapi.py --failurerate 10
+python3 configure-cc-sapi.py --failurerate 10 --delayrange 100-500
 rm -f jmeter_results.csv
 test -d webreport && chmod 777 webreport
 rm -rf webreport
